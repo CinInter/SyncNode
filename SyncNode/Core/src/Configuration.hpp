0ccb5b38d339b48ac3cd94458969107d3b64cee0
faissal <faissal.elosman@gmail.com>
2017-03-06 21:52:52 +0100

    Python tests fix 7

--- SyncNode/Core/src/Configuration.hpp
+++ SyncNode/Core/src/Configuration.hpp
@@ -11,8 +11,11 @@
 #define	IDENTIFICATION_THRESHOLD 0.8
 #define NB_OF_THREADS 3
 #define	SAMPLING_RATE 8000
-//#define DEVICE_MAC_ADDRESS "f4:5c:89:c5:5e:af\0" // mac address of MacBook
-#define DEVICE_MAC_ADDRESS "b8:27:eb:45:90:ad\0" // mac address of RPi3
+#ifndef __arm__
+#define DEVICE_MAC_ADDRESS "f4:5c:89:c5:5e:af\0"
+#else
+#define DEVICE_MAC_ADDRESS "b8:27:eb:45:90:ad\0"
+#endif
 
 #define FOUR_SEC_BUFF_TIME_IN_MICRO_SEC 4000000
 #define PCM_NAME "plughw:CARD=duo,DEV=0"
